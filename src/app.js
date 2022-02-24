091447fc9af24e8e268883d1853ad07960db075c
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2022-02-24 02:52:16 -0500

    added innerHTML

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-let apiKey = "50cfd6595523a7f69104e698dea7cff4";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=new york&appid=${apiKey}&units==metric`;
+function displayTemp(response) {
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let speedElement = document.querySelector("#speed");
+console.log(response.data);
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+speedElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
-console.log(apiUrl);d
+let apiKey = "50cfd6595523a7f69104e698dea7cff4";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=new york&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
