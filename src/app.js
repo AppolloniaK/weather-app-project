0806325397f6fd28f47e4bcd147b840bc57d011e
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2022-02-24 02:06:50 -0500

    add units to apiUrl

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 let apiKey = "50cfd6595523a7f69104e698dea7cff4";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=new york&appid=${apiKey}&units==metric`;
 
-console.log(apiUrl);
+console.log(apiUrl);d
