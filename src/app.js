8768fb12605a2e8b8e0be446d38c3cca3430123d
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2022-02-24 22:48:39 -0500

    added current date and time

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 function formatDate(timestamp) {
 let date = new Date(timestamp);
 let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`
+}
 let minutes = date.getMinutes();
-let day = date.getDate();
-return = `${day} ${hours}:${minutes}`;
+if (minutes < 10) {
+  minutes = `0${minutes}`
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemp(response) {
+  console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let speedElement = document.querySelector("#speed");
-let dateElement = document.quertSelector("#date");
-
-
-console.log(response.data);
+let dateElement = document.querySelector("#date");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
