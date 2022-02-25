aebf8b3d5211cfc99715159f75d9842b9bae0522
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2022-02-24 22:12:07 -0500

    adding Last Updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let day = date.getDate();
+return = `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let speedElement = document.querySelector("#speed");
+let dateElement = document.quertSelector("#date");
+
+
 console.log(response.data);
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
+humidityElement.innerHTML = Math.round(response.data.main.humidity);
 speedElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "50cfd6595523a7f69104e698dea7cff4";
