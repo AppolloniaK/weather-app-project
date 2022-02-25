0d07d5752f833d832323b1d95953b1952eab329c
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2022-02-24 23:42:50 -0500

    changed atl to autodate icon description

--- src/app.js
+++ src/app.js
@@ -14,21 +14,26 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemp(response) {
+  console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let speedElement = document.querySelector("#speed");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = Math.round(response.data.main.humidity);
 speedElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "50cfd6595523a7f69104e698dea7cff4";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=new york&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
